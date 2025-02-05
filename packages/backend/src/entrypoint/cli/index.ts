5ef02d6265f27f32b8ccaea2a734be9a99213d1f
ss49919201 <sakashin10291029@gmail.com>
2025-02-06 08:37:50 +0900

    feat: Add cli interface

---
+++ packages/backend/src/entrypoint/cli/index.ts
@@ -0,0 +1,28 @@
+import { stdin, stdout } from "node:process";
+import { createInterface } from "node:readline/promises";
+import { findManyMontoWithPage } from "../../infrastructure/db/json/monto";
+
+const readlineInterface = createInterface({
+  input: stdin,
+  output: stdout,
+});
+
+const main = async () => {
+  // TODO: implement other questions
+  const page = await readlineInterface.question(
+    "ページ番号を入力してください: "
+  );
+
+  const result = findManyMontoWithPage({
+    page: Number(page),
+    firstName: null,
+    lastName: null,
+    dateOfDeath: null,
+    homyo: null,
+    ingou: null,
+  });
+
+  console.log(result);
+};
+
+main();
