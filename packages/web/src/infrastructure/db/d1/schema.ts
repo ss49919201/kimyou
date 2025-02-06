8be0a28ec0f96c9b441fab82edac9b823e0564a0
ss49919201 <sakashin10291029@gmail.com>
2025-02-07 08:33:54 +0900

    refactor: Rename fields in montos and buddhistProfiles tables for consistency

--- packages/web/src/infrastructure/db/d1/schema.ts
+++ packages/web/src/infrastructure/db/d1/schema.ts
@@ -2,22 +2,22 @@ import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const montos = sqliteTable("montos", {
   id: text().notNull().unique(),
-  first_name: text().notNull(),
-  last_name: text().notNull(),
-  address: text().notNull(),
-  date_of_death: text(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
-  created_date: text().notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
-  updated_date: text().notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
+  firstName: text("first_name").notNull(),
+  lastName: text("last_name").notNull(),
+  address: text("address").notNull(),
+  dateOfDeath: text("daetOfDeath"), // RFC3339 ex)2006-01-02T15:04:05Z07:00
+  createdDate: text("created_date").notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
+  updatedDate: text("updated_date").notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
 });
 
 export const buddhistProfiles = sqliteTable("buddhist_profiles", {
   id: text().notNull().unique(),
-  monto_id: text()
+  montoId: text("monto_id")
     .notNull()
     .unique()
     .references(() => montos.id),
   homyo: text().notNull(),
   ingou: text(),
-  created_date: text().notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
-  updated_date: text().notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
+  createdDate: text("created_date").notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
+  updatedDate: text("updated_date").notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
 });
