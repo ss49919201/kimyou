70eb49cf31301e84ea6c80386da78c6b220ee18c
ss49919201 <sakashin10291029@gmail.com>
2025-02-06 23:38:50 +0900

    refactor: Rename database tables for consistency and clarity

--- packages/web/src/infrastructure/db/d1/schema.ts
+++ packages/web/src/infrastructure/db/d1/schema.ts
@@ -1,22 +1,16 @@
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const administrator = sqliteTable("administrator", {
-  id: text().notNull().unique(),
-  hased_password: text().notNull(),
-  created_date: text().notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
-  updated_date: text().notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
-});
-
-export const monto = sqliteTable("monto", {
+export const monto = sqliteTable("montos", {
   id: text().notNull().unique(),
   first_name: text().notNull(),
   last_name: text().notNull(),
+  address: text().notNull(),
   date_of_death: text(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
   created_date: text().notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
   updated_date: text().notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
 });
 
-export const buddhistProfile = sqliteTable("buddhist_profile", {
+export const buddhistProfile = sqliteTable("buddhist_profiles", {
   id: text().notNull().unique(),
   monto_id: text()
     .notNull()
