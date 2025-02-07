2d3e9393dd7476acbe27bb1aa359d86ae0699d5b
ss49919201 <sakashin10291029@gmail.com>
2025-02-08 01:24:15 +0900

    feat: Add search form for filtering montos by first name in the monto list page

--- packages/web/src/pages/montos/index.tsx
+++ packages/web/src/pages/montos/index.tsx
@@ -18,6 +18,33 @@ const MontoList = ({ values: montos }: MontosWithPage) => (
     <div className="flex justify-between items-center mb-6">
       <h1 className="text-2xl font-bold">門徒一覧</h1>
     </div>
+
+    <form method="get" action="/montos" class="max-w-md mb-6">
+      <label
+        for="default-search"
+        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+      >
+        Search
+      </label>
+      <div class="relative flex">
+        <input
+          type="search"
+          name="firstName"
+          id="default-search"
+          class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="門徒名を入力してください"
+          required
+        />
+        <div class="w-4"></div>
+        <button
+          type="submit"
+          class="w-32 text-white end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          検索
+        </button>
+      </div>
+    </form>
+
     <div className="overflow-x-auto bg-white rounded-lg shadow">
       <table className="w-full">
         <thead className="bg-gray-50">
