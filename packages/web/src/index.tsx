fe6f5085de764932ae4a2c118884b59ee85db98a
ss49919201 <sakashin10291029@gmail.com>
2025-02-07 09:14:53 +0900

    feat: Implement basic authentication middleware and add credentials to local configuration

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -2,12 +2,23 @@ import { Hono } from "hono";
 import Montos from "./pages/montos";
 import { drizzle } from "drizzle-orm/d1";
 import { findManyWithPage } from "./infrastructure/db/d1/monto";
+import { basicAuth } from "hono/basic-auth";
 
 type Bindings = {
   D1: D1Database;
+  BASIC_USERNAME: string;
+  BASIC_PASSWORD: string;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
+
+app.use("/*", async (c, next) => {
+  const auth = basicAuth({
+    username: c.env.BASIC_USERNAME,
+    password: c.env.BASIC_PASSWORD,
+  });
+  return auth(c, next);
+});
 
 app.get("/montos", async (c) => {
   // TODO: implement me
