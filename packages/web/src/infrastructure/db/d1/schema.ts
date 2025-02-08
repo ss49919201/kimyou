8dd0b281c6dbf9a3946315d2786cd88f346a90de
ss49919201 <sakashin10291029@gmail.com>
2025-02-08 18:47:08 +0900

    feat: Add genders table and update montos schema to include genderId and phoneNumber fields

--- packages/web/src/infrastructure/db/d1/schema.ts
+++ packages/web/src/infrastructure/db/d1/schema.ts
@@ -1,9 +1,18 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const genders = sqliteTable("genders", {
+  id: integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
+  type: text("type").notNull(),
+  createdDate: text("created_date").notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
+  updatedDate: text("updated_date").notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
+});
 
 export const montos = sqliteTable("montos", {
-  id: text().notNull().unique(),
+  id: text().notNull().primaryKey(),
+  genderId: integer({ mode: "number" }).references(() => genders.id),
   firstName: text("first_name").notNull(),
   lastName: text("last_name").notNull(),
+  phoneNumber: text("phone_number"),
   address: text("address").notNull(),
   dateOfDeath: text("date_of_death"), // RFC3339 ex)2006-01-02T15:04:05Z07:00
   createdDate: text("created_date").notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
@@ -11,7 +20,7 @@ export const montos = sqliteTable("montos", {
 });
 
 export const buddhistProfiles = sqliteTable("buddhist_profiles", {
-  id: text().notNull().unique(),
+  id: text().notNull().primaryKey(),
   montoId: text("monto_id")
     .notNull()
     .unique()
