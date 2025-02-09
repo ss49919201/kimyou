4afe4a4951dea3d288151671adc0ec6e7ddd92a0
ss49919201 <sakashin10291029@gmail.com>
2025-02-10 08:40:15 +0900

    feat: Add Monto model with gender and optional fields, and update schema to allow nullable homyo

---
+++ packages/batch/src/domain/model/monto.ts
@@ -0,0 +1,17 @@
+export const genders = ["男", "女"] as const;
+export type Gender = (typeof genders)[number];
+
+export type UnsavedMonto = {
+  gender: Gender;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  address?: string;
+  dateOfDeath?: Date;
+  homyo?: string;
+  ingou?: string;
+};
+
+export type SavedHomyo = UnsavedMonto & {
+  id: string;
+};
