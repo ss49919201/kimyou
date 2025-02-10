0147cc6d9a46deed4e2f3c3dff8db0af72163d9e
ss49919201 <sakashin10291029@gmail.com>
2025-02-10 22:33:12 +0900

    refactor: Refactor input type for insertMontos to improve structure and data types

--- packages/batch/src/usecase/insertMontos.ts
+++ packages/batch/src/usecase/insertMontos.ts
@@ -1,25 +1,18 @@
-import * as v from "valibot";
 import { UnsavedMonto } from "../domain/model/monto";
 
-const wetRun = v.boolean();
-
-export const input = v.object({
-  wetRun: wetRun,
-  montos: v.array(
-    v.object({
-      gender: v.union([v.literal("男"), v.literal("女")]),
-      firstName: v.string(),
-      lastName: v.string(),
-      phoneNumber: v.string(),
-      address: v.string(),
-      dateOfDeath: v.optional(v.string()),
-      homyo: v.optional(v.string()),
-      ingou: v.optional(v.string()),
-    })
-  ),
-});
-
-export type Input = v.InferInput<typeof input>;
+export type Input = {
+  wetRun: boolean;
+  mongos: {
+    gender: "MAN" | "WOMEN";
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    address: string;
+    dateOfDeath?: Date;
+    homyo?: string;
+    ingou?: string;
+  }[];
+};
 
 export type Dependency = {
   insertMonto: (unsavedMonto: UnsavedMonto) => Promise<void>;
