b40a3b6ad8cbaf81ae7bb385df2761dfc0806a6d
ss49919201 <sakashin10291029@gmail.com>
2025-02-10 22:49:00 +0900

    feat: Enhance Monto model validation with Valibot and update types

--- packages/batch/src/domain/model/monto.ts
+++ packages/batch/src/domain/model/monto.ts
@@ -1,17 +1,28 @@
+import * as v from "valibot";
+
 export const genders = ["MAN", "WOMEN"] as const;
 export type Gender = (typeof genders)[number];
 
-export type UnsavedMonto = {
-  gender: Gender;
-  firstName: string;
-  lastName: string;
-  phoneNumber: string;
-  address: string;
-  dateOfDeath?: Date;
-  homyo?: string;
-  ingou?: string;
-};
+const monto = v.object({
+  gender: v.picklist(genders),
+  firstName: v.string(),
+  lastName: v.string(),
+  phoneNumber: v.string(),
+  address: v.string(),
+  dateOfDeath: v.optional(v.date()),
+  homyo: v.optional(v.string()),
+  ingou: v.optional(v.string()),
+});
 
-export type SavedHomyo = UnsavedMonto & {
-  id: string;
-};
+export const unsavedMonto = monto;
+
+export type UnsavedMonto = v.InferOutput<typeof unsavedMonto>;
+
+export const savedMonto = v.intersect([
+  monto,
+  v.object({
+    id: v.pipe(v.string(), v.uuid("The UUID is badly formatted.")),
+  }),
+]);
+
+export type SavedHomyo = v.InferOutput<typeof savedMonto>;
