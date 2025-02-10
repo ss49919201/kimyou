94e28efd806f2b2890c324a7b99ad8ec6bd4ecb2
ss49919201 <sakashin10291029@gmail.com>
2025-02-10 23:37:34 +0900

    feat: Integrate insertMontos function to process montos from CSV input

--- packages/batch/src/insert-montos/index.ts
+++ packages/batch/src/insert-montos/index.ts
@@ -1,6 +1,8 @@
 import { program } from "commander";
 import * as v from "valibot";
+import { UnsavedMonto } from "../domain/model/monto";
 import { generateMontosFromCsv } from "../infrastructure/csv/monto";
+import { insertMontos } from "../usecase/insertMontos";
 
 async function main() {
   program
@@ -12,7 +14,25 @@ async function main() {
 
       const montosFromCsv = await generateMontosFromCsv(parsedFile);
 
-      console.log(montosFromCsv);
+      await insertMontos(
+        {
+          wetRun: parsedWetRun,
+          montos: montosFromCsv.map((monto) => ({
+            gender: monto.性別 === "男" ? "MAN" : "WOMEN",
+            firstName: monto.名,
+            lastName: monto.性,
+            phoneNumber: monto.電話番号,
+            address: monto.住所,
+            dateOfDeath: monto.命日 ? new Date(monto.命日) : undefined,
+            homyo: monto.法名,
+            ingou: monto.院号,
+          })),
+        },
+        {
+          // mock
+          insertMonto: async (_: UnsavedMonto) => {},
+        }
+      );
     });
 
   program.parse();
