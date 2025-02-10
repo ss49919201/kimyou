574f14e1461f439d007f91631b07ce94df0cc542
ss49919201 <sakashin10291029@gmail.com>
2025-02-10 09:13:54 +0900

    feat: Refactor input schema to encapsulate Monto data as an array in insertMontos function

--- packages/batch/src/usecase/insertMontos.ts
+++ packages/batch/src/usecase/insertMontos.ts
@@ -4,14 +4,18 @@ const wetRun = v.boolean();
 
 export const input = v.object({
   wetRun: wetRun,
-  gender: v.union([v.literal("男"), v.literal("女")]),
-  firstName: v.string(),
-  lastName: v.string(),
-  phoneNumber: v.string(),
-  address: v.string(),
-  dateOfDeath: v.optional(v.string()),
-  homyo: v.optional(v.string()),
-  ingou: v.optional(v.string()),
+  montos: v.array(
+    v.object({
+      gender: v.union([v.literal("男"), v.literal("女")]),
+      firstName: v.string(),
+      lastName: v.string(),
+      phoneNumber: v.string(),
+      address: v.string(),
+      dateOfDeath: v.optional(v.string()),
+      homyo: v.optional(v.string()),
+      ingou: v.optional(v.string()),
+    })
+  ),
 });
 
 export type Input = v.InferInput<typeof input>;
