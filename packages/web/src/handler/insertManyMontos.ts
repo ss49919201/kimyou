0fa61fb0243a39989b39a62edfbd2096de6f4d0b
ss49919201 <sakashin10291029@gmail.com>
2025-02-15 10:13:42 +0900

    feat: Add handler for inserting multiple Montos and refactor related code

---
+++ packages/web/src/handler/insertManyMontos.ts
@@ -0,0 +1,39 @@
+import { vValidator } from "@hono/valibot-validator";
+import { drizzle } from "drizzle-orm/d1";
+import * as v from "valibot";
+import { genders, UnsavedMonto } from "../domain/model/monto";
+import { insertMonto } from "../infrastructure/db/d1/monto";
+import { insertManyMontos as insertManyMontosUsecase } from "../usecase/insertManyMontos";
+import { factory } from "./factory";
+
+export const insertManyMontos = factory.createHandlers(
+  vValidator(
+    "json",
+    v.object({
+      wetRun: v.boolean(),
+      montos: v.array(
+        v.object({
+          gender: v.picklist(genders),
+          firstName: v.string(),
+          lastName: v.string(),
+          phoneNumber: v.string(),
+          address: v.string(),
+          dateOfDeath: v.optional(v.date()),
+          homyo: v.optional(v.string()),
+          ingou: v.optional(v.string()),
+        })
+      ),
+    })
+  ),
+  async (c) => {
+    const params = c.req.valid("json");
+    const db = drizzle(c.env.D1, { logger: true });
+
+    await insertManyMontosUsecase(params, {
+      insertMonto: (unsavedMonto: UnsavedMonto) =>
+        insertMonto(db, unsavedMonto),
+    });
+
+    return c.json({ msg: "ok" });
+  }
+);
--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,16 +1,13 @@
 import { Hono } from "hono";
 import GenerateHomyo from "./pages/homyos/generate";
-import { drizzle } from "drizzle-orm/d1";
-import { insertMonto } from "./infrastructure/db/d1/monto";
 import { vValidator } from "@hono/valibot-validator";
 import * as v from "valibot";
 import { generateHomyos } from "./infrastructure/ai/workersAi/homyo";
-import { insertMontos } from "./usecase/insertMontos";
-import { genders, UnsavedMonto } from "./domain/model/monto";
 import { Bindings } from "./handler/bindings";
 import { findManyMontosHandler } from "./handler/findManyMontosHandler";
 import { findOneMontoHandler } from "./handler/findOneMonto";
 import { indexHandler } from "./handler";
+import { insertManyMontos } from "./handler/insertManyMontos";
 
 const app = new Hono<{ Bindings: Bindings }>();
 
@@ -20,38 +17,7 @@ app.get("/montos", ...findManyMontosHandler);
 
 app.get("/montos/:id", ...findOneMontoHandler);
 
-app.post(
-  "/montos/_batch",
-  vValidator(
-    "json",
-    v.object({
-      wetRun: v.boolean(),
-      montos: v.array(
-        v.object({
-          gender: v.picklist(genders),
-          firstName: v.string(),
-          lastName: v.string(),
-          phoneNumber: v.string(),
-          address: v.string(),
-          dateOfDeath: v.optional(v.date()),
-          homyo: v.optional(v.string()),
-          ingou: v.optional(v.string()),
-        })
-      ),
-    })
-  ),
-  async (c) => {
-    const params = c.req.valid("json");
-    const db = drizzle(c.env.D1, { logger: true });
-
-    await insertMontos(params, {
-      insertMonto: (unsavedMonto: UnsavedMonto) =>
-        insertMonto(db, unsavedMonto),
-    });
-
-    return c.json({ msg: "ok" });
-  }
-);
+app.post("/montos/_batch", ...insertManyMontos);
 
 app.get(
   "/homyos/generate",
