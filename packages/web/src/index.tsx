c0ee488b0accf59fb66e8609e9dfe39cc86be9d9
ss49919201 <sakashin10291029@gmail.com>
2025-02-15 10:16:05 +0900

    feat: Add generateHomyo handler and refactor route integration

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,13 +1,10 @@
 import { Hono } from "hono";
-import GenerateHomyo from "./pages/homyos/generate";
-import { vValidator } from "@hono/valibot-validator";
-import * as v from "valibot";
-import { generateHomyos } from "./infrastructure/ai/workersAi/homyo";
 import { Bindings } from "./handler/bindings";
 import { findManyMontosHandler } from "./handler/findManyMontosHandler";
 import { findOneMontoHandler } from "./handler/findOneMonto";
 import { indexHandler } from "./handler";
 import { insertManyMontos } from "./handler/insertManyMontos";
+import { generateHomyo } from "./handler/generateHomyo";
 
 const app = new Hono<{ Bindings: Bindings }>();
 
@@ -19,24 +16,6 @@ app.get("/montos/:id", ...findOneMontoHandler);
 
 app.post("/montos/_batch", ...insertManyMontos);
 
-app.get(
-  "/homyos/generate",
-  vValidator(
-    "query",
-    v.object({
-      "first-name": v.optional(v.string()),
-    })
-  ),
-  async (c) => {
-    const { "first-name": firstName } = c.req.valid("query");
-    const homyos: string[] = [];
-
-    if (firstName) {
-      homyos.push(...(await generateHomyos(c.env.ENV, c.env.AI, firstName)));
-    }
-
-    return c.html(<GenerateHomyo {...{ homyos }} />);
-  }
-);
+app.get("/homyos/generate", ...generateHomyo);
 
 export default app;
