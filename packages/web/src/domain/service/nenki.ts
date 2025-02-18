8bd032e49c96684251c1b4f065922bdad17df962
ss49919201 <ss49919201@gmail.com>
2025-02-18 11:41:44 +0900

    fix: improve calcNextNenki logic and add additional test cases

--- packages/web/src/domain/service/nenki.ts
+++ packages/web/src/domain/service/nenki.ts
@@ -1,19 +1,19 @@
-import { addYears } from "date-fns";
+import { addYears, isAfter } from "date-fns";
 
+/**
+ * @name calcNextNenki
+ * @param dateOfDeath - The original date of death
+ * @returns The calculated date, or undefined if current date is after next neki.
+ * @description
+ * Return nenki in the future that is closest to the current date and time.
+ */
 export function calcNextNenki(dateOfDeath: Date): Date | undefined {
   const now = new Date();
-
-  const yearsToAdd = [1, 2, 6, 13, 16, 24, 33, 50];
-  for (const [idx, yearToAdd] of yearsToAdd.entries()) {
-    const last = idx === yearsToAdd.length - 1;
-    const nenki = addYears(dateOfDeath, yearToAdd);
-
-    if (nenki >= now && !last) {
-      return nenki;
-    }
-  }
-
-  return void 0;
+  const yearsToAdd = [1, 2, 6, 12, 16, 24, 32, 49];
+  const nenkis = yearsToAdd.map((yearToAdd) =>
+    addYears(dateOfDeath, yearToAdd)
+  );
+  return nenkis.find((nenki) => isAfter(nenki, now));
 }
 
 // in-source test suites
@@ -29,19 +29,39 @@ if (import.meta.vitest) {
       vi.useRealTimers();
     });
 
-    it("Should return calculated date", () => {
-      vi.setSystemTime(new Date("1970-02-10T15:00:00Z"));
+    it("Should return calculated date (1th)", () => {
+      const dateOfDeath = new Date("1970-01-10T15:00:00Z");
+      const oneMonthAfterDateOfDeath = new Date("1970-02-10T15:00:00Z");
+      vi.setSystemTime(oneMonthAfterDateOfDeath);
 
-      const date = new Date("1970-01-10T15:00:00Z");
-      const result = calcNextNenki(date);
+      const result = calcNextNenki(dateOfDeath);
       expect(result).toEqual(new Date("1971-01-10T15:00:00Z"));
     });
 
-    it("Should return undefined after 50th", () => {
-      vi.setSystemTime(new Date("2020-01-10T15:00:00Z"));
+    it("Should return calculated date (3th)", () => {
+      const dateOfDeath = new Date("1970-01-10T15:00:00Z");
+      const oneYearAfterDateOfDeath = new Date("1971-01-10T15:00:00Z");
+      vi.setSystemTime(oneYearAfterDateOfDeath);
 
-      const date = new Date("1970-01-10T15:00:00Z");
-      const result = calcNextNenki(date);
+      const result = calcNextNenki(dateOfDeath);
+      expect(result).toEqual(new Date("1972-01-10T15:00:00Z"));
+    });
+
+    it("Should return calculated date (50th)", () => {
+      const dateOfDeath = new Date("1970-01-10T15:00:00Z");
+      const thirtyTwoYearsAfterDateOfDeath = new Date("2002-01-10T15:00:00Z");
+      vi.setSystemTime(thirtyTwoYearsAfterDateOfDeath);
+
+      const result = calcNextNenki(dateOfDeath);
+      expect(result).toEqual(new Date("2019-01-10T15:00:00Z"));
+    });
+
+    it("Should return undefined after 50th", () => {
+      const dateOfDeath = new Date("1970-01-10T15:00:00Z");
+      const fiftyYearsAfterDateOfDeath = new Date("2020-01-10T15:00:00Z");
+      vi.setSystemTime(fiftyYearsAfterDateOfDeath);
+
+      const result = calcNextNenki(dateOfDeath);
       expect(result).toBeUndefined();
     });
   });
