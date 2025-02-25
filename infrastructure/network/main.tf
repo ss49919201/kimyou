21eb98b3080aba10b5c13d6187582eaf66ca0b24
Shinya Sakae <ss49919201@gmail.com>
2025-02-25 23:55:10 +0900

    feat: Add Cloudflare variables and resources for Zero Trust access

--- infrastructure/network/main.tf
+++ infrastructure/network/main.tf
@@ -11,12 +11,58 @@ terraform {
   }
 }
 
+variable "cloudflare_account_id" {
+  type = string
+}
+
 variable "cloudflare_api_token" {
   type = string
 }
 
-# Use `-var` or `-var-file`
-# e.g. `terraform apply -var="cloudflare_api_token=xxx"`
+variable "allow_emails" {
+  type = list(string)
+}
+
+variable "domain" {
+  type = string
+}
+
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
+}
+
+resource "cloudflare_zero_trust_access_service_token" "this" {
+  account_id = var.cloudflare_account_id
+  name = "kimyou-api-auth"
+}
+
+
+resource "cloudflare_zero_trust_access_application" "this" {
+  account_id = var.cloudflare_account_id
+  domain     = var.domain
+  type       = "self_hosted"
+  policies = [
+    {
+      precedence = 1
+      name       = "kimyou-service-auth-policy"
+      decision   = "non_identity"
+      include = [
+        {
+          service_token = {
+            token_id = cloudflare_zero_trust_access_service_token.this.id
+          }
+        },
+      ]
+    },
+    {
+      precedence = 2
+      name       = "kimyou-allow-policy"
+      decision   = "allow"
+      include = [for allow_email in var.allow_emails : {
+        email = {
+          email = allow_email
+        }
+      }]
+    },
+  ]
 }
