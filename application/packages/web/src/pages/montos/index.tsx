f656793b0d21367474c583bce647c2c78220645d
Shinya Sakae <ss49919201@gmail.com>
2025-02-26 23:15:56 +0900

    reafactor: Update href attribute for new Monto registration link to use absolute path

--- application/packages/web/src/pages/montos/index.tsx
+++ application/packages/web/src/pages/montos/index.tsx
@@ -45,7 +45,7 @@ const MontoList = ({ values: montos }: MontosWithPage) => (
         </button>
       </form>
       <a
-        href={"montos/new"}
+        href="/montos/new"
         className="flex justify-center items-center text-white end-2.5 bottom-2.5 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
       >
         新規登録
