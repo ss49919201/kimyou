b6f8b894f29113a09866bda8dce7c175c88b53e9
Shinya Sakae <ss49919201@gmail.com>
2025-02-27 00:13:07 +0900

    fix: Fix gender condition in monto insertion query

--- application/packages/web/src/infrastructure/db/d1/monto.ts
+++ application/packages/web/src/infrastructure/db/d1/monto.ts
@@ -52,7 +52,7 @@ export async function insertMonto(
   const selectedGender = await db
     .select({ id: genders.id })
     .from(genders)
-    .where(eq(genders.type, unsavedMonto.gender === "MALE" ? "男" : "女"))
+    .where(eq(genders.type, unsavedMonto.gender))
     .get();
 
   if (!selectedGender) {
