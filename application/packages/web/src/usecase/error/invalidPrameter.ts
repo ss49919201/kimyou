2cd49fe11179862c3565c4d983b426af3c064628
Shinya Sakae <ss49919201@gmail.com>
2025-02-26 23:16:02 +0900

    fix: Make details parameter optional in InvalidParameterError constructor

--- application/packages/web/src/usecase/error/invalidPrameter.ts
+++ application/packages/web/src/usecase/error/invalidPrameter.ts
@@ -1,8 +1,8 @@
 export class InvalidParameterError extends Error {
   type = "InvalidParameter";
-  readonly details: string;
+  readonly details?: string;
 
-  constructor(msg: string, details: string) {
+  constructor(msg: string, details?: string) {
     super(msg);
     this.details = details;
   }
