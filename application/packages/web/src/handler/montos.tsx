655dd178e354f4f5de8ff3cf53a6e750880c7eb8
Shinya Sakae <ss49919201@gmail.com>
2025-02-26 23:31:51 +0900

    refactor: Update import to use findManyMontosWithPage in montos handler

--- application/packages/web/src/handler/montos.tsx
+++ application/packages/web/src/handler/montos.tsx
@@ -3,7 +3,7 @@ import { vValidator } from "@hono/valibot-validator";
 import * as v from "valibot";
 import { drizzle } from "drizzle-orm/d1";
 import { factory } from "./factory";
-import { findManyWithPage } from "../infrastructure/db/d1/monto";
+import { findManyMontosWithPage } from "../infrastructure/db/d1/montoReader";
 import { vValidatorHook } from "./vValidator";
 
 export const montos = factory.createHandlers(
@@ -18,7 +18,7 @@ export const montos = factory.createHandlers(
     const { "last-name": lastName } = c.req.valid("query");
 
     const db = drizzle(c.env.D1, { logger: true });
-    const result = await findManyWithPage(db, { lastName });
+    const result = await findManyMontosWithPage(db, { lastName });
 
     return c.render(<Montos {...result} />);
   }
