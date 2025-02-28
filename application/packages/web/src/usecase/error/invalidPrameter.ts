4a7d28417685acb2155ba56af890532065b460f9
Shinya Sakae <ss49919201@gmail.com>
2025-03-01 00:53:20 +0900

    feat: Enhance error messages by including details in InvalidParameterError and NotFoundError constructors

--- application/packages/web/src/usecase/error/invalidPrameter.ts
+++ application/packages/web/src/usecase/error/invalidPrameter.ts
@@ -3,6 +3,9 @@ export class InvalidParameterError extends Error {
   readonly details?: string;
 
   constructor(msg: string, details?: string) {
+    if (details) {
+      msg += ` details: ${details}`;
+    }
     super(msg);
     this.details = details;
   }
