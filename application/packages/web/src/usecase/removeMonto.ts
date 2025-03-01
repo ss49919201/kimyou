eb13047e94b93ec3a2a3b2c39cb149290aa70d2c
Shinya Sakae <ss49919201@gmail.com>
2025-03-01 11:54:48 +0900

    feat: Implement locking mechanism in removeMonto and restoreMonto to prevent concurrent updates

--- application/packages/web/src/usecase/removeMonto.ts
+++ application/packages/web/src/usecase/removeMonto.ts
@@ -3,8 +3,10 @@ import {
   inactiveSavedMonto,
   SavedMonto,
 } from "../domain/model/monto";
+import { ConflictError } from "./error/conflict";
 import { InvalidParameterError } from "./error/invalidPrameter";
 import { NotFoundError } from "./error/notFound";
+import { lockKeyForUpdateMonto } from "./lockKey";
 
 export type Input = {
   id: string;
@@ -14,12 +16,30 @@ export type Input = {
 export type Dependency = {
   findMonto: (id: string) => Promise<SavedMonto | undefined>;
   updateMonto: (savedMonto: SavedMonto) => Promise<void>;
+  lock: (key: string) => Promise<boolean>;
+  unlock: (key: string) => Promise<void>;
 };
 
 export async function removeMonto(
   input: Input,
   dep: Dependency
 ): Promise<void> {
+  const lockKey = lockKeyForUpdateMonto(input.id);
+  if (!(await dep.lock(lockKey))) {
+    throw new ConflictError("Monto update alreacy locked.");
+  }
+  try {
+    await _removeMonto(input, dep);
+  } finally {
+    try {
+      await dep.unlock(lockKey);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+async function _removeMonto(input: Input, dep: Dependency): Promise<void> {
   const monto = await dep.findMonto(input.id);
   if (!monto) {
     throw new NotFoundError("monto not found");
--- application/packages/web/src/usecase/restoreMonto.ts
+++ application/packages/web/src/usecase/restoreMonto.ts
@@ -1,6 +1,8 @@
 import { SavedMonto, activeSavedMonto } from "../domain/model/monto";
+import { ConflictError } from "./error/conflict";
 import { InvalidParameterError } from "./error/invalidPrameter";
 import { NotFoundError } from "./error/notFound";
+import { lockKeyForUpdateMonto } from "./lockKey";
 
 export type Input = {
   id: string;
@@ -9,12 +11,30 @@ export type Input = {
 export type Dependency = {
   findMonto: (id: string) => Promise<SavedMonto | undefined>;
   updateMonto: (savedMonto: SavedMonto) => Promise<void>;
+  lock: (key: string) => Promise<boolean>;
+  unlock: (key: string) => Promise<void>;
 };
 
 export async function restoreMonto(
   input: Input,
   dep: Dependency
 ): Promise<void> {
+  const lockKey = lockKeyForUpdateMonto(input.id);
+  if (!(await dep.lock(lockKey))) {
+    throw new ConflictError("Monto update alreacy locked.");
+  }
+  try {
+    await _restoreMonto(input, dep);
+  } finally {
+    try {
+      await dep.unlock(lockKey);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+async function _restoreMonto(input: Input, dep: Dependency): Promise<void> {
   const monto = await dep.findMonto(input.id);
   if (!monto) {
     throw new NotFoundError("monto not found");
