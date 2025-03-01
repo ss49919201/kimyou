96d6ea3ff0af95ebc7a55bd8e92188af841afdfb
Shinya Sakae <ss49919201@gmail.com>
2025-03-02 00:46:25 +0900

    test: Add E2E snapshot tests for new monto creation route

--- application/packages/web/e2e/index.test.ts
+++ application/packages/web/e2e/index.test.ts
@@ -16,9 +16,47 @@ describe("E2E Snapshot test", () => {
     { route: `/montos/${dummyMontoId}/edit` },
     { route: `/montos/${dummyInvalidMontoId}` },
     { route: "/montos/new" },
-  ])("route `$route`", async ({ route }) => {
-    const req = new Request(`http://localhost:8787${route}`);
-    const res = await worker.fetch(req, env, createExecutionContext());
+    {
+      route: "/montos/new",
+      method: "POST",
+      formData: {
+        gender: "MALE",
+        address: "address",
+        "first-name": "fn",
+        "last-name": "ln",
+        "phone-number": "0311112222",
+        "date-of-death": "2025-01-01",
+        homyo: "釋一宗",
+        ingou: "帰命院",
+      },
+    },
+  ])("route `$route`", async ({ route, method, formData }) => {
+    let body;
+    if (formData) {
+      body = new FormData();
+      for (const [name, value] of Object.entries(formData)) {
+        body.append(name, value);
+      }
+    }
+
+    const baseUrl = "http://localhost:8787";
+    const req = new Request(`${baseUrl}${route}`, {
+      method,
+      body,
+      headers: {
+        origin: baseUrl,
+      },
+    });
+
+    let res = await worker.fetch(req, env, createExecutionContext());
+    if (res.status === 302 && res.headers.get("location")) {
+      res = await worker.fetch(
+        new Request(`${baseUrl}${res.headers.get("location")}`),
+        env,
+        createExecutionContext()
+      );
+    }
+
     const resRext = await res.text();
     expect(resRext).toMatchSnapshot();
   });
