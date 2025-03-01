167f549d16c8f052af1c43f51800eab847927ff4
Shinya Sakae <ss49919201@gmail.com>
2025-03-02 00:50:44 +0900

    test: Remove E2E snapshot tests for monto new post and simplify request handling

--- application/packages/web/e2e/page.test.ts
+++ application/packages/web/e2e/page.test.ts
@@ -16,47 +16,10 @@ describe("E2E Snapshot test", () => {
     { route: `/montos/${dummyMontoId}/edit` },
     { route: `/montos/${dummyInvalidMontoId}` },
     { route: "/montos/new" },
-    {
-      route: "/montos/new",
-      method: "POST",
-      formData: {
-        gender: "MALE",
-        address: "address",
-        "first-name": "fn",
-        "last-name": "ln",
-        "phone-number": "0311112222",
-        "date-of-death": "2025-01-01",
-        homyo: "釋一宗",
-        ingou: "帰命院",
-      },
-    },
-  ])("route `$route`", async ({ route, method, formData }) => {
-    let body;
-    if (formData) {
-      body = new FormData();
-      for (const [name, value] of Object.entries(formData)) {
-        body.append(name, value);
-      }
-    }
-
+  ])("route `$route`", async ({ route }) => {
     const baseUrl = "http://localhost:8787";
-    const req = new Request(`${baseUrl}${route}`, {
-      method,
-      body,
-      headers: {
-        origin: baseUrl,
-      },
-    });
-
-    let res = await worker.fetch(req, env, createExecutionContext());
-    if (res.status === 302 && res.headers.get("location")) {
-      res = await worker.fetch(
-        new Request(`${baseUrl}${res.headers.get("location")}`),
-        env,
-        createExecutionContext()
-      );
-    }
-
+    const req = new Request(`${baseUrl}${route}`);
+    const res = await worker.fetch(req, env, createExecutionContext());
     const resRext = await res.text();
     expect(resRext).toMatchSnapshot();
   });
