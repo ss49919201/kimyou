94c5fdca600a3680ddbf439cecbf70bc858cf0b1
Shinya Sakae <ss49919201@gmail.com>
2025-03-01 12:17:55 +0900

    feat: Remove side effect

--- application/packages/web/src/handler/api/removeMonto.ts
+++ application/packages/web/src/handler/api/removeMonto.ts
@@ -1,14 +1,6 @@
 import { vValidator } from "@hono/valibot-validator";
-import { drizzle } from "drizzle-orm/d1";
-import { HTTPException } from "hono/http-exception";
 import * as v from "valibot";
-import { inactiveMontoReason, SavedMonto } from "../../domain/model/monto";
-import { lock, unlock } from "../../infrastructure/db/d1/lock";
-import {
-  findOneForUpdate,
-  updateMonto,
-} from "../../infrastructure/db/d1/monto";
-import { removeMonto as removeMontoUsecase } from "../../usecase/removeMonto";
+import { inactiveMontoReason } from "../../domain/model/monto";
 import { factory } from "../factory";
 import { vValidatorHook } from "../vValidator";
 
@@ -21,30 +13,6 @@ export const removeMonto = factory.createHandlers(
     vValidatorHook()
   ),
   async (c) => {
-    const id = c.req.param("id");
-
-    if (!id) {
-      throw new HTTPException(500, {
-        message: "path parameter id is not found in monto handler",
-      });
-    }
-
-    const params = c.req.valid("json");
-    const db = drizzle(c.env.D1, { logger: true });
-
-    await removeMontoUsecase(
-      {
-        ...params,
-        id,
-      },
-      {
-        findMonto: (id) => findOneForUpdate(db, id),
-        updateMonto: (savedMonto: SavedMonto) => updateMonto(db, savedMonto),
-        lock: (key) => lock(db, key),
-        unlock: (key) => unlock(db, key),
-      }
-    );
-
     return c.json({ msg: "ok" });
   }
 );
--- application/packages/web/src/handler/api/restoreMonto.ts
+++ application/packages/web/src/handler/api/restoreMonto.ts
@@ -1,34 +1,5 @@
-import { drizzle } from "drizzle-orm/d1";
-import { HTTPException } from "hono/http-exception";
-import { SavedMonto } from "../../domain/model/monto";
-import { lock, unlock } from "../../infrastructure/db/d1/lock";
-import {
-  findOneForUpdate,
-  updateMonto,
-} from "../../infrastructure/db/d1/monto";
-import { restoreMonto as restoreMontoUsecase } from "../../usecase/restoreMonto";
 import { factory } from "../factory";
 
 export const restoreMonto = factory.createHandlers(async (c) => {
-  const id = c.req.param("id");
-
-  if (!id) {
-    throw new HTTPException(500, {
-      message: "path parameter id is not found in monto handler",
-    });
-  }
-
-  const db = drizzle(c.env.D1, { logger: true });
-
-  await restoreMontoUsecase(
-    { id },
-    {
-      findMonto: (id) => findOneForUpdate(db, id),
-      updateMonto: (savedMonto: SavedMonto) => updateMonto(db, savedMonto),
-      lock: (key) => lock(db, key),
-      unlock: (key) => unlock(db, key),
-    }
-  );
-
   return c.json({ msg: "ok" });
 });
