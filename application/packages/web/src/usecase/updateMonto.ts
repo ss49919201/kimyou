faaf2e98c27d54289b8ebcd82151a21c4fe8ff62
Shinya Sakae <ss49919201@gmail.com>
2025-03-01 11:47:02 +0900

    feat: Implement locking in updateMonto to prevent concurrent updates

--- application/packages/web/src/usecase/updateMonto.ts
+++ application/packages/web/src/usecase/updateMonto.ts
@@ -1,6 +1,8 @@
 import { SavedMonto, modifiedSavedMonto } from "../domain/model/monto";
+import { ConflictError } from "./error/conflict";
 import { InvalidParameterError } from "./error/invalidPrameter";
 import { NotFoundError } from "./error/notFound";
+import { lockKeyForUpdateMonto } from "./lockKey";
 
 export type Input = {
   id: string;
@@ -14,12 +16,30 @@ export type Input = {
 export type Dependency = {
   findMonto: (id: string) => Promise<SavedMonto | undefined>;
   updateMonto: (savedMonto: SavedMonto) => Promise<void>;
+  lock: (key: string) => Promise<boolean>;
+  unock: (key: string) => Promise<void>;
 };
 
 export async function updateMonto(
   input: Input,
   dep: Dependency
 ): Promise<void> {
+  const lockKey = lockKeyForUpdateMonto(input.id);
+  if (!(await dep.lock(lockKey))) {
+    throw new ConflictError("Monto update alreacy locked.");
+  }
+  try {
+    await _updateMonto(input, dep);
+  } finally {
+    try {
+      await dep.unock(lockKey);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+async function _updateMonto(input: Input, dep: Dependency): Promise<void> {
   const monto = await dep.findMonto(input.id);
   if (!monto) {
     throw new NotFoundError("monto not found");
