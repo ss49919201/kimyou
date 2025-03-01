c947b1a49e674800b256617c27a68acde99b494c
Shinya Sakae <ss49919201@gmail.com>
2025-03-02 01:07:38 +0900

    test: Enhance E2E page tests with additional POST route handling and request domain configuration

--- application/packages/web/e2e/page.test.ts
+++ application/packages/web/e2e/page.test.ts
@@ -8,6 +8,8 @@ describe("E2E Page test", () => {
     await setupD1(env.D1, env.D1_MIGRATIONS);
   });
 
+  const domain = "http://localhost:8787";
+
   it.each([
     { route: "/" },
     { route: "/homyos/generate" },
@@ -17,9 +19,51 @@ describe("E2E Page test", () => {
     { route: `/montos/${dummyInvalidMontoId}` },
     { route: "/montos/new" },
   ])("route `$route`", async ({ route }) => {
-    const req = new Request(`http://localhost:8787${route}`);
+    const req = new Request(`${domain}${route}`);
     const res = await worker.fetch(req, env, createExecutionContext());
     const resRext = await res.text();
     expect(resRext).toMatchSnapshot();
+  });
+
+  it.each([
+    {
+      route: `/montos/${dummyMontoId}/edit`,
+      formData: {
+        "phone-number": "0311112222",
+        "date-of-death": "2025-01-01",
+        homyo: "釋一海",
+        ingou: "帰命院",
+        address: "大町",
+      },
+      expected: { redirectPath: "/montos" },
+    },
+    {
+      route: "/montos/new",
+      formData: {
+        "first-name": "テスト",
+        "last-name": "太郎",
+        "phone-number": "0311112222",
+        "date-of-death": "2025-01-01",
+        homyo: "釋一海",
+        ingou: "帰命院",
+        gender: "MALE",
+        address: "大町",
+      },
+      expected: { redirectPath: "/montos" },
+    },
+  ])("route `$route` POST", async ({ route, formData, expected }) => {
+    const body = new FormData();
+    for (const [name, value] of Object.entries(formData)) {
+      body.append(name, value);
+    }
+    console.log(body);
+    const req = new Request(`${domain}${route}`, {
+      method: "POST",
+      body,
+      headers: { origin: domain },
+    });
+    const res = await worker.fetch(req, env, createExecutionContext());
+    expect(res.status).toBe(302);
+    expect(res.headers.get("location")).toBe(expected.redirectPath);
   });
 });
