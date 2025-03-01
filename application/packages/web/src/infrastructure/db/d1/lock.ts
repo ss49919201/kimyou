c65f181c70ef689a8def57af89c220d61f9403ce
Shinya Sakae <ss49919201@gmail.com>
2025-03-01 11:46:46 +0900

    feat: Rename getLock to lock and update to return boolean for lock status

--- application/packages/web/src/infrastructure/db/d1/lock.ts
+++ application/packages/web/src/infrastructure/db/d1/lock.ts
@@ -2,17 +2,36 @@ import { eq } from "drizzle-orm";
 import { DrizzleD1Database } from "drizzle-orm/d1";
 import { locks } from "./schema";
 
-export async function getLock(
+/**
+ * @description
+ * Return false if already locked.
+ */
+export async function lock(
   db: DrizzleD1Database,
   key: string
-): Promise<void> {
-  await db.insert(locks).values({
-    key,
-    lockedDate: new Date().toISOString(),
-  });
+): Promise<boolean> {
+  return await db
+    .insert(locks)
+    .values({
+      key,
+      lockedDate: new Date().toISOString(),
+    })
+    .then(() => true)
+    .catch((e: unknown) => {
+      // D1 does not throw extended error class when unique constraint error occurs.
+      if (
+        e instanceof Error &&
+        e.message ===
+          "D1_ERROR: UNIQUE constraint failed: locks.key: SQLITE_CONSTRAINT"
+      ) {
+        return false;
+      }
+
+      throw e;
+    });
 }
 
-export async function releaseLock(
+export async function unlock(
   db: DrizzleD1Database,
   key: string
 ): Promise<void> {
