f7b47cf78df8e1000f8d17214137c9004cb598fd
Shinya Sakae <ss49919201@gmail.com>
2025-03-01 17:28:27 +0900

    feat: Add initial gender data to the database

---
+++ application/packages/web/src/infrastructure/db/d1/master/genders.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+	"genders" ("type", "created_date", "updated_date")
+VALUES
+	('MALE', strftime('%Y-%m-%dT%H:%M:%fZ'), strftime('%Y-%m-%dT%H:%M:%fZ')),
+	('FEMALE', strftime('%Y-%m-%dT%H:%M:%fZ'), strftime('%Y-%m-%dT%H:%M:%fZ'));
