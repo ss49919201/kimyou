aa2fe7f2021e64e3ab58a2e4c5d429f32e9470b5
Shinya Sakae <ss49919201@gmail.com>
2025-03-02 00:54:05 +0900

    fix: Correct request URL construction in E2E page test

--- application/packages/web/e2e/page.test.ts
+++ application/packages/web/e2e/page.test.ts
@@ -17,7 +17,7 @@ describe("E2E Page test", () => {
     { route: `/montos/${dummyInvalidMontoId}` },
     { route: "/montos/new" },
   ])("route `$route`", async ({ route }) => {
-    const req = new Request(`http://localhost:8787"${route}`);
+    const req = new Request(`http://localhost:8787${route}`);
     const res = await worker.fetch(req, env, createExecutionContext());
     const resRext = await res.text();
     expect(resRext).toMatchSnapshot();
