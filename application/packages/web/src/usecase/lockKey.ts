bb92e31293896d069a929163d763cd4c0da07e83
Shinya Sakae <ss49919201@gmail.com>
2025-03-02 10:01:47 +0900

    feat: add validation for lockKeyForUpdateMonto function

--- application/packages/web/src/usecase/lockKey.ts
+++ application/packages/web/src/usecase/lockKey.ts
@@ -1,3 +1,10 @@
+/**
+ * @param monto id of 1 or more characters
+ * @returns lock key for update monto
+ */
 export function lockKeyForUpdateMonto(id: string): `updateMonto:${string}` {
+  if (id.length < 1) {
+    throw new Error("Monto id length must be greater than equal to 1");
+  }
   return `updateMonto:${id}`;
 }
