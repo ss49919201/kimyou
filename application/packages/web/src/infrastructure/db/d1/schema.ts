563492444dba0a68bdd6875da882757282ccbac3
Shinya Sakae <ss49919201@gmail.com>
2025-03-11 22:28:51 +0900

    feat: Define empty schema for address_groups table in database

--- application/packages/web/src/infrastructure/db/d1/schema.ts
+++ application/packages/web/src/infrastructure/db/d1/schema.ts
@@ -78,3 +78,10 @@ export const restoreMontos = sqliteTable("restore_montos", {
     .references(() => montos.id),
   restoredDate: text("restored_date").notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
 });
+
+// TODO: define schema
+export const addressGroup = sqliteTable("address_groups", {
+  id: text().notNull().primaryKey(),
+  createdDate: text("created_date").notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
+  updatedDate: text("updated_date").notNull(), // RFC3339 ex)2006-01-02T15:04:05Z07:00
+});
